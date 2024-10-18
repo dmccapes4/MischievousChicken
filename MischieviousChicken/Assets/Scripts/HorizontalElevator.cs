3f6896f26919bd396b9ae5107439f9cc0717fa5f
subzerox334 <subzerox334@gmail.com>
2024-10-17 22:56:55 -0700

    bunch of stuff lvl2

---
+++ MischieviousChicken/Assets/Scripts/HorizontalElevator.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class MovingPlankHorizontal : MonoBehaviour
+{
+    public float moveDistance = 5.0f;
+    public float moveSpeed = 2.0f;
+
+    private Vector3 startPosition;
+    private bool isMovingRight = true;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        if (isMovingRight)
+        {
+            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+
+            if (transform.position.x >= startPosition.x + moveDistance)
+            {
+                transform.position = startPosition + Vector3.right * moveDistance;
+                isMovingRight = false;
+            }
+        }
+        else
+        {
+            transform.position -= Vector3.right * moveSpeed * Time.deltaTime;
+
+            if (transform.position.x <= startPosition.x)
+            {
+                transform.position = startPosition;
+                isMovingRight = true;
+            }
+        }
+    }
+}
