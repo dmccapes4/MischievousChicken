14cae3a48ac529bdf34b8b73bfa51f21a65262bb
subzerox334 <subzerox334@gmail.com>
2024-10-17 21:46:41 -0700

    a bunch of stuff

--- MischieviousChicken/Assets/Scripts/PlayerMovement.cs
+++ MischieviousChicken/Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,9 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb;
     private SpriteRenderer spriteRenderer;
 
+    private int jumpCount = 0;
+    private const int maxJumps = 2;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -17,11 +20,8 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
-
-        // Apply horizontal movement
         rb.velocity = new Vector2(horizontalInput * speed, rb.velocity.y);
 
-        // Flip the sprite when moving left
         if (horizontalInput < 0)
         {
             spriteRenderer.flipX = true;
@@ -31,16 +31,39 @@ public class PlayerController : MonoBehaviour
             spriteRenderer.flipX = false;
         }
 
-        // Apply jump force when jump button is pressed
-        if (Input.GetButtonDown("Jump") && IsGrounded())
+        if (Input.GetButtonDown("Jump") && jumpCount < maxJumps)
         {
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            jumpCount++;
         }
     }
 
-    bool IsGrounded()
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        // Check if the player's collider is touching any colliders on the "Ground" layer
-        return rb.IsTouchingLayers(LayerMask.GetMask("Ground"));
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            jumpCount = 0;
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            Vector2 collisionNormal = collision.contacts[0].normal;
+
+            if (Mathf.Abs(collisionNormal.x) > 0.1f)
+            {
+                rb.velocity = new Vector2(0, rb.velocity.y);
+            }
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            jumpCount = 0;
+        }
     }
 }
